c4d06db59bbe285031ffb28c7f6a1522f128f26b
neals-sudo <59893889+neals-sudo@users.noreply.github.com>
2024-02-25 21:19:13 +0100

    Update injector.py

--- injector.py
+++ injector.py
@@ -1,22 +1,29 @@
-import frida, sys
+import frida
+import sys
+import argparse
 
 jscodeFile = open("sniffer.js", "r")
 jscode = jscodeFile.read()
 jscodeFile.close()
 
 def on_message(message, data):
-    if message['type'] == 'send':
-        print(message['payload'])
-    else:
-        print(message)
+  if message['type'] == 'send':
+    print(message['payload'])
+  else:
+    print(message)
 
-device = frida.get_usb_device()
+if __name__ == '__main__':
+    # Argument Parsing
+    parser = argparse.ArgumentParser(description="Frida script runner for network interception.")
+    parser.add_argument("app_id", help="The package name (app ID) of the target Android application.") 
+    args = parser.parse_args()
 
-# insert app id here
-process = device.attach("com.example.app")
-script = process.create_script(jscode)
-script.on('message', on_message)
+    device = frida.get_usb_device()
 
-print('[+] Running')
-script.load()
-sys.stdin.read()
+    process = device.attach(args.app_id)  # Use the argument
+    script = process.create_script(jscode)
+    script.on('message', on_message)
+
+    print('[+] Running')
+    script.load()
+    sys.stdin.read()
